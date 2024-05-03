69ce2a6de7713bc4a1bc5b18fff3bcb654b53dee
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-03 17:15:14 +0200

    .

--- src/main/java/ui/BacteriaPopulationPanel.java
+++ src/main/java/ui/BacteriaPopulationPanel.java
@@ -1,4 +1,21 @@
 package ui;
 
-public class BacteriaPopulationPanel {
+import model.BacteriaPopulation;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BacteriaPopulationPanel extends JPanel {
+    private BacteriaPopulation bacteriaPopulation;
+
+    public BacteriaPopulationPanel(BacteriaPopulation bacteriaPopulation) {
+        this.bacteriaPopulation = bacteriaPopulation;
+        setLayout(new BorderLayout());
+
+        // Display the details of the bacteria population
+        JTextArea detailsTextArea = new JTextArea();
+        detailsTextArea.setEditable(false);
+        detailsTextArea.setText(bacteriaPopulation.toString());
+        add(detailsTextArea, BorderLayout.CENTER);
+    }
 }
