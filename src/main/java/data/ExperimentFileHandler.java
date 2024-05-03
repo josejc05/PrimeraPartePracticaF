51ca8a0d62df2a3def30400b7e51331b27a05e9b
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-03 17:13:14 +0200

    .

--- src/main/java/data/ExperimentFileHandler.java
+++ src/main/java/data/ExperimentFileHandler.java
@@ -1,4 +1,21 @@
 package data;
 
+import model.Experiment;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExperimentFileHandler {
+    public static void saveExperiment(Experiment experiment, String filename) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+            out.writeObject(experiment);
+        }
+    }
+
+    public static Experiment loadExperiment(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+            return (Experiment) in.readObject();
+        }
+    }
 }
