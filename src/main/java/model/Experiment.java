09fb2c6357c1d0708b02d16bd959bcaa13a9db3b
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-03 17:14:54 +0200

    .

--- src/main/java/model/Experiment.java
+++ src/main/java/model/Experiment.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Experiment {
+    private List<BacteriaPopulation> bacteriaPopulations;
+
+    public Experiment() {
+        this.bacteriaPopulations = new ArrayList<>();
+    }
+
+    public void addBacteriaPopulation(BacteriaPopulation bacteriaPopulation) {
+        this.bacteriaPopulations.add(bacteriaPopulation);
+    }
+
+    public void removeBacteriaPopulation(BacteriaPopulation bacteriaPopulation) {
+        this.bacteriaPopulations.remove(bacteriaPopulation);
+    }
+
+    public List<BacteriaPopulation> getBacteriaPopulations() {
+        return bacteriaPopulations;
+    }
 }
