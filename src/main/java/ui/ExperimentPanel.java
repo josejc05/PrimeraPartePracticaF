8db273350727cd011c81dbf7031559af7d59e3a8
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-17 20:13:44 +0200

    .

--- src/main/java/ui/ExperimentPanel.java
+++ src/main/java/ui/ExperimentPanel.java
@@ -7,8 +7,6 @@ import data.ExperimentFileHandler;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
